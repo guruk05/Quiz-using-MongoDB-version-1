ac9084964901ae0a1ae8d7c47246ff0d8b8520bd
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-10-16 12:32:23 +0530

    Initialzed function to get switch between questions

--- fun.js
+++ fun.js
@@ -15,6 +15,21 @@ $(document).ready(function(){
            getQues(main);
        }); 
 })
+// Display data 
+function getQues(main){
+    
+       if(currentQuestion < 9){  
+           $('#ques-div').text(main[currentQuestion].question);
+           $('#option1').text(main[currentQuestion].options[0]);
+           $('#option2').text(main[currentQuestion].options[1]);
+           $('#option3').text(main[currentQuestion].options[2]);
+           $('#option4').text(main[currentQuestion].options[3]);
+           $('.displayScore').hide();
+           correctAns = main[currentQuestion].answer;
+           currentQuestion++;
+       }
+}
+// Store selected answer in array
 
 
 
