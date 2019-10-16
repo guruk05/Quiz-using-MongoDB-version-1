fe9d7783e4673d2fd202f3fe13fcfd972c9c1ded
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-10-16 12:31:24 +0530

    Using ajax to get data

--- fun.js
+++ fun.js
@@ -4,6 +4,17 @@ let arrStore=[];
 let correctAns;
 let answer;
 let main;
+// fetch data from mongodb database
+$(document).ready(function(){
+   $.ajax({
+       url:'http://localhost:8100/api',
+   }).done(function(data) {
+       main = data;
+       getQues(main);
+       $('#next').click(function(){
+           getQues(main);
+       }); 
+})
 
 
 
